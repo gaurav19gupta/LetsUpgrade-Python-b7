0f0f244e2e3fbeb38ab97fcccf2e1e07332a1051
gaurav19gupta <70703945+gaurav19gupta@users.noreply.github.com>
2020-09-11 23:58:52 +0530

    Day4-Assignment
    Armstrong Number

---
+++ Assignment1-Day4.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Armstrong Number
+# 
+
+# In[6]:
+
+
+lower = 1042000
+upper = 702648265
+for num in range(lower,upper+1):
+    order = len(str(num))
+    sum = 0
+    temp = num
+    
+    while temp>0:
+        digit = temp % 10
+        sum += digit ** order
+        temp //= 10
+    if num == sum:
+        print("The first armstrong number is ", num)
+        break
+        
+        
+    
+
+
+# In[ ]:
+
+
+
+
